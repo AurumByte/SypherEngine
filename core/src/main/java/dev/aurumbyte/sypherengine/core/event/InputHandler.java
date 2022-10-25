2b9ffd5bd68ef8f1780e9ba7279104c891faaf71
AurumByte <80045511+AurumByte@users.noreply.github.com>
2022-10-25 15:39:24 +0530

    Add files via upload

---
+++ core/src/main/java/dev/aurumbyte/sypherengine/core/event/InputHandler.java
@@ -0,0 +1,12 @@
+/* (C)2022 AurumByte */
+package dev.aurumbyte.sypherengine.core.event;
+
+public class InputHandler {
+    public KeyListener keyListener;
+    public MouseListener mouseListener;
+
+    public InputHandler(KeyListener keyListener, MouseListener mouseListener) {
+        this.keyListener = keyListener;
+        this.mouseListener = mouseListener;
+    }
+}
---
+++ ecs/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module io.github.sypherengine.ecs {
+    exports dev.aurumbyte.sypherengine.ecs;
+    exports dev.aurumbyte.sypherengine.ecs.component;
+    exports dev.aurumbyte.sypherengine.ecs.entity;
+    exports dev.aurumbyte.sypherengine.ecs.system;
+}
